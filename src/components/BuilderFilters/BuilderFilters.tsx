932e5e44f98c47a09d651477895a0a3d49cfac4f
airfortech <airm@xxx.yy>
2023-01-09 15:03:11 +0100

    added show/hide filters toggle

--- src/components/BuilderFilters/BuilderFilters.tsx
+++ src/components/BuilderFilters/BuilderFilters.tsx
@@ -1,75 +1,87 @@
 import { Product } from "../../types/Product";
-import dimensionsImage from "../../assets/img/dimensions.jpg";
+import { useState } from "react";
 import { BuilderFiltersInput } from "./BuilderFiltersInput/BuilderFiltersInput";
+import dimensionsImage from "../../assets/img/dimensions.jpg";
+import arrowImg from "../../assets/img/icon-arrow-down.svg";
 
 interface Props {
   setProducts: React.Dispatch<React.SetStateAction<Product[]>>;
 }
 
 export const BuilderFilters = ({ setProducts }: Props) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleShowFilters = () => {
+    setIsVisible(prevState => !prevState);
+  };
+
   return (
     <div className={"cd-builder-filters"}>
-      <h2>Dobierz produkt dla Siebie</h2>
-      <p>
-        TATALU™ – od teraz spacery z Twoim dzieckiem są jeszcze bardziej
-        komfortowe! Nowa odsłona wózka spacerowego, którego lekka i wytrzymała
-        konstrukcja doskonale sprawdzi się w każdych warunkach, a forma
-        parasolki pozwala na szybkie złożenie i przechowywanie go.
-      </p>
-      <div>
-        <img src={dimensionsImage} alt="wymiary" />
-        <form action="">
-          <legend>podaj wymiary:</legend>
-          <BuilderFiltersInput
-            name="WSize"
-            placeholder="Wymiar W"
-            unit="cm"
-            value={0}
-            setProducts={setProducts}
+      <div className="header">
+        <h2>Dobierz produkt dla Siebie</h2>
+        <button className="toggle" onClick={handleShowFilters}>
+          <img
+            className={isVisible ? "" : "up"}
+            src={arrowImg}
+            alt="pokaż / ukryj"
           />
-          {/* <BuilderFiltersInput
-            name="weight"
-            placeholder="Waga"
-            unit="kg"
-            value={0}
-            setProducts={setProducts}
-          /> */}
-          <BuilderFiltersInput
-            name="ESize"
-            placeholder="Wymiar E"
-            unit="cm"
-            value={0}
-            setProducts={setProducts}
-          />
-          <BuilderFiltersInput
-            name="ASize"
-            placeholder="Wymiar A"
-            unit="cm"
-            value={0}
-            setProducts={setProducts}
-          />
-          <BuilderFiltersInput
-            name="CSize"
-            placeholder="Wymiar C"
-            unit="cm"
-            value={0}
-            setProducts={setProducts}
-          />
-          <BuilderFiltersInput
-            name="FSize"
-            placeholder="Wymiar F"
-            unit="cm"
-            value={0}
-            setProducts={setProducts}
-          />
-          <BuilderFiltersInput
-            name="LSize"
-            placeholder="Wymiar L"
-            unit="cm"
-            value={0}
-            setProducts={setProducts}
-          />
-        </form>
+        </button>
+      </div>
+      <div style={{ display: isVisible ? "block" : "none" }}>
+        <p>
+          TATALU™ – od teraz spacery z Twoim dzieckiem są jeszcze bardziej
+          komfortowe! Nowa odsłona wózka spacerowego, którego lekka i wytrzymała
+          konstrukcja doskonale sprawdzi się w każdych warunkach, a forma
+          parasolki pozwala na szybkie złożenie i przechowywanie go.
+        </p>
+        <div className="form-container">
+          <img src={dimensionsImage} alt="wymiary" />
+          <form action="">
+            <legend>podaj wymiary:</legend>
+            <BuilderFiltersInput
+              name="WSize"
+              placeholder="Wymiar W"
+              unit="cm"
+              value={0}
+              setProducts={setProducts}
+            />
+            <BuilderFiltersInput
+              name="ESize"
+              placeholder="Wymiar E"
+              unit="cm"
+              value={0}
+              setProducts={setProducts}
+            />
+            <BuilderFiltersInput
+              name="ASize"
+              placeholder="Wymiar A"
+              unit="cm"
+              value={0}
+              setProducts={setProducts}
+            />
+            <BuilderFiltersInput
+              name="CSize"
+              placeholder="Wymiar C"
+              unit="cm"
+              value={0}
+              setProducts={setProducts}
+            />
+            <BuilderFiltersInput
+              name="FSize"
+              placeholder="Wymiar F"
+              unit="cm"
+              value={0}
+              setProducts={setProducts}
+            />
+            <BuilderFiltersInput
+              name="LSize"
+              placeholder="Wymiar L"
+              unit="cm"
+              value={0}
+              setProducts={setProducts}
+            />
+          </form>
+        </div>
       </div>
     </div>
   );
